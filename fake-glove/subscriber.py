4f38c4f36c292c3e8ed68a6c879a25db4777339b
João Diogo Craveiro Ferreira <devilj@outlook.pt>
2025-03-22 12:48:05 +0000

    fake-glove: Implementar argumentos da linha de comandos.

--- fake-glove/subscriber.py
+++ fake-glove/subscriber.py
@@ -1,6 +1,24 @@
 import paho.mqtt.subscribe as subscribe
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-a', '--addr', type=str, default="test.mosquitto.org",
+    help="Endereço do broker MQTT."
+)
+parser.add_argument(
+    '-p', '--port', type=int, default=1883,
+    help="Porto do broker MQTT."
+)
+
+args = parser.parse_args()
+
 
 def on_message(client, userdata, message: subscribe.paho.MQTTMessage):
     print(f"topic: {message.topic}\npayload: {message.payload}")
 
-subscribe.callback(on_message, "case/raw/phys", hostname="test.mosquitto.org", port=1883)
+subscribe.callback(
+    on_message,
+    "case/raw/phys",
+    hostname=args.addr, port=args.port
+)
