c8219306ffaefb66a65cb794bbd0962facecaf85
Rubem Neto <joserubemn31@gmail.com>
2025-05-31 23:15:25 +0100

    fix live monitoring dashboards

--- frontend/src/features/monitoring/components/Dashboards/index.tsx
+++ frontend/src/features/monitoring/components/Dashboards/index.tsx
@@ -5,7 +5,7 @@ export default function Dashboard({deviceId, fields}: {deviceId: string, fields:
         <div key={field}>
           <div className="flex flex-col gap-2">
             <iframe
-              src={`http://localhost:3000/d-solo/d4ebeaef-38b9-48e3-9d38-7d80cfeb9260/iot?orgId=1&from=1748725735468&to=1748725795468&timezone=browser&var-deviceId=${deviceId}&var-field=${field}&panelId=2&__feature.dashboardSceneSolo`}
+              src={`http://localhost:3000/d-solo/d4ebeaef-38b9-48e3-9d38-7d80cfeb9260/iot?orgId=1&timezone=browser&var-deviceId=${deviceId}&var-field=${field}&panelId=1&__feature.dashboardSceneSolo`}
               height={400}
               className="w-full border-none"
               loading="lazy"
--- frontend/src/features/monitoring/components/HistoricalDashboards/index.tsx
+++ frontend/src/features/monitoring/components/HistoricalDashboards/index.tsx
@@ -7,7 +7,7 @@ export default function HistoricalDashboards({deviceId, patientId, experimentId,
           <div key={field}>
             <div className="flex flex-col gap-2">
                 <iframe
-                  src={`http://localhost:3000/d-solo/dae0e31d-8150-44f7-a661-e02885189d0b/historical-data?orgId=1&from=1748726041776&to=1748726341776&timezone=browser&var-subjectId=${patientId}&var-deviceId=${deviceId}&var-experimentId=${experimentId}&var-field=${field}&panelId=4&__feature.dashboardSceneSolo`}
+                  src={`http://localhost:3000/d-solo/dae0e31d-8150-44f7-a661-e02885189d0b/historical-data?orgId=1&timezone=browser&var-subjectId=${patientId}&var-deviceId=${deviceId}&var-experimentId=${experimentId}&var-field=${field}&panelId=4&__feature.dashboardSceneSolo`}
                   height={400}
                   className="w-full border-none"
                   loading="lazy"
@@ -22,7 +22,7 @@ export default function HistoricalDashboards({deviceId, patientId, experimentId,
           <div key={field}>
             <div className="flex flex-col gap-2">
                 <iframe
-                  src={`http://localhost:3000/d-solo/dae0e31d-8150-44f7-a661-e02885189d0b/historical-data?orgId=1&from=1748727850203&to=1748728150203&timezone=browser&var-subjectId=${patientId}&var-deviceId=${deviceId}&var-experimentId=${experimentId}&var-field=bvp&var-processing_field=${field}&panelId=7&__feature.dashboardSceneSolo`}
+                  src={`http://localhost:3000/d-solo/dae0e31d-8150-44f7-a661-e02885189d0b/historical-data?orgId=1&timezone=browser&var-subjectId=${patientId}&var-deviceId=${deviceId}&var-experimentId=${experimentId}&var-field=bvp&var-processing_field=${field}&panelId=7&__feature.dashboardSceneSolo`}
                   height={400}
                   className="w-full border-none"
                   loading="lazy"
